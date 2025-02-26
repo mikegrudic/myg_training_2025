1896fde5fcf1fa2104e204fa967a2846ea544e08
Mike Grudic <mike.grudic@gmail.com>
2025-02-26 14:04:44 -0500

    feat: heartrate vs pace plot

--- hr_vs_pace.py
+++ hr_vs_pace.py
@@ -3,20 +3,38 @@ from glob import glob
 import numpy as np
 from matplotlib import pyplot as plt
 from astropy import units as u
+from datetime import datetime, timedelta
+from palettable.cmocean.sequential import Haline_6_r
+
+cmap = plt.get_cmap("rainbow_r")
+cmap_weeks = 6
 
 runs = glob("runs/*.fit")
 num_runs = len(runs)
-
+fig, ax = plt.subplots()
+# ax.set_prop_cycle("color", Dark2_7.mpl_colors)
+t0 = datetime.today().date()
+ts = []
 for i, f in enumerate(sorted(runs)):
     values, units = fitfile_to_data(f, smoothing_seconds=60.0, seconds_tocut=0)
 
     distances = values["distance"]
+    #    print(distances.max())
+    if distances.max() < 4.9e3:  # or distances.max() > 6e3:
+        continue
     heartrates = values["heart_rate"]
     timestamps = values["timestamp"]
+    if timestamps[-1].date() < t0:
+        t0 = timestamps[-1].date()
+    dt = datetime.today().date() - timestamps[-1].date()
+    dt_program = timestamps[-1].date() - datetime(2025, 1, 1).date()
+    # print(dt.days)
+    # if datetime.today().date() - timestamps[-1].date() > timedelta(days=31):
+    #     continue
     seconds_to_cut = 300
     if np.any(np.diff(distances) < 0):
         continue
-    speed = np.gradient(smooth(distances, 60.0))[seconds_to_cut:]
+    speed = np.gradient(smooth(distances, 60.0))  # [seconds_to_cut:]
     if not len(speed):
         continue
 
@@ -24,22 +42,34 @@ for i, f in enumerate(sorted(runs)):
     if np.any(speed == 0):
         pace[speed == 0] = np.inf
 
-    sigma_pace = 0.5 * np.diff(np.percentile(pace, [16, 84]))[0]
-    if sigma_pace > 4:
-        continue
-    sigma_hr = 0.5 * np.diff(np.percentile(heartrates[seconds_to_cut:], [16, 84]))[0]
-
-    plt.errorbar(
+    pace = pace[distances < 5e3]
+    heartrates = heartrates[distances < 5e3]
+    sigma_pace = np.diff(np.percentile(pace[seconds_to_cut:], [16, 50, 84]))[:, None]
+    sigma_hr = np.diff(np.percentile(heartrates[seconds_to_cut:], [16, 50, 84]))[:, None]
+    #    print(sigma_hr)
+    #   print(np.percentile(heartrates[seconds_to_cut:], [16, 50, 84]), sigma_hr)
+    # ax.hist(heartrates[seconds_to_cut:])
+    print(dt_program)
+    ax.errorbar(
         np.median(heartrates[seconds_to_cut:]),
         np.median(pace[seconds_to_cut:]),
         yerr=sigma_pace,
         xerr=sigma_hr,
-        label=timestamps[-1].date(),
-        lw=1,
+        lw=0.8,
         marker="s",
+        markersize=4,
+        color=cmap(dt.days / 7 / cmap_weeks),
+        zorder=-dt.days,
+        alpha=(0.5 if dt.days > 42 else 1.0),
     )
+#    ts.append(dt.days / 7)
+
+s = ax.scatter(np.zeros_like(ts), np.zeros_like(ts), c=ts, vmin=0, vmax=cmap_weeks, cmap=cmap)
+plt.colorbar(s, label="Lookback Time (Weeks)", pad=0)
+
+ax.set(xlim=[145, 170], ylim=[8, 11])
 plt.ylabel("Pace (min/mi)")
 plt.xlabel("Heart Rate (bpm)")
 # plt.xlim(120,180)
-plt.legend(labelspacing=0, frameon=True, fontsize=8)
+# plt.legend(labelspacing=0, frameon=True, fontsize=8)
 plt.savefig("heartrate_vs_speed.pdf", bbox_inches="tight")

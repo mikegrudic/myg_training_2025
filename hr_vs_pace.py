78ae080d824f3969d0ae146608e88f14f8135d44
Mike Grudic <mike.grudic@gmail.com>
2025-05-15 09:59:01 -0400

    add grade adjustment

--- hr_vs_pace.py
+++ hr_vs_pace.py
@@ -14,17 +14,23 @@ race_day = datetime(2025, 9, 6)
 runs = glob("runs_block2/*.fit")
 num_runs = len(runs)
 
+
+def grade_adjustment(grade_percent):
+    grade, fac = np.loadtxt("strava_GAP_table.dat").T
+    return np.interp(grade_percent, grade, fac)
+
+
 for dist in 5, 10, 21:
     fig, ax = plt.subplots()
     # ax.set_prop_cycle("color", Dark2_7.mpl_colors)
     t0 = datetime.today().date()
     ts = []
-    for i, f in enumerate(sorted(runs)):
+    for i, f in enumerate(reversed(sorted(runs))):
         values, units = fitfile_to_data(f, smoothing_seconds=60.0, seconds_tocut=0)
 
         distances = values["distance"]
-        print(dist, distances.max())
-        if distances.max() < 1e3 * dist * 0.97:  # or distances.max() > 6e3:
+        altitude = values["enhanced_altitude"]
+        if distances.max() < 1e3 * dist * 0.97:
             continue
         heartrates = values["heart_rate"]
         timestamps = values["timestamp"]
@@ -32,17 +38,26 @@ for dist in 5, 10, 21:
             t0 = timestamps[-1].date()
         dt = race_day.date() - datetime.today().date()  # - timestamps[-1].date()
         dt_race = datetime(2025, 5, 3).date() - timestamps[-1].date()
-        # print(dt.days)
-        # if datetime.today().date() - timestamps[-1].date() > timedelta(days=31):
-        #     continue
+
         seconds_to_cut = 300
         if np.any(np.diff(distances) < 0):
             continue
-        speed = np.gradient(smooth(distances, 60.0))  # [seconds_to_cut:]
+
+        SPEED_SMOOTHING_SECONDS = 10
+        speed = np.gradient(smooth(distances, SPEED_SMOOTHING_SECONDS))
+        vertical_speed = np.gradient(smooth(altitude, SPEED_SMOOTHING_SECONDS))
+        grade_percent = vertical_speed / speed * 100
+        gap_factor = grade_adjustment(grade_percent)
+
+        # print(f)
+        # plt.plot(grade_percent)
+        # plt.show()
+        # continue
+        # # exit()
         if not len(speed):
             continue
 
-        pace = 1 / (speed * u.m.to(u.imperial.mile) * 60)
+        pace = 1 / (speed * u.m.to(u.imperial.mile) * 60) / gap_factor
         if np.any(speed == 0):
             pace[speed == 0] = np.inf
 
@@ -50,10 +65,7 @@ for dist in 5, 10, 21:
         heartrates = heartrates[distances < 1e3 * dist]
         sigma_pace = np.diff(np.percentile(pace[seconds_to_cut:], [16, 50, 84]))[:, None]
         sigma_hr = np.diff(np.percentile(heartrates[seconds_to_cut:], [16, 50, 84]))[:, None]
-        #    print(sigma_hr)
-        #   print(np.percentile(heartrates[seconds_to_cut:], [16, 50, 84]), sigma_hr)
-        # ax.hist(heartrates[seconds_to_cut:])
-        # print(dt_program)
+
         ax.errorbar(
             np.median(heartrates[seconds_to_cut:]),
             np.median(pace[seconds_to_cut:]),
@@ -74,7 +86,7 @@ for dist in 5, 10, 21:
     # plt.colorbar(s, label="Weeks Until Race Day", pad=0)
     ax.set_title(f"{dist}k Runs")
     ax.set(xlim=[145, 170], ylim=[7, 12])
-    plt.ylabel("Pace (min/mi)")
+    plt.ylabel("Grade-adjusted pace (min/mi)")
     plt.xlabel("Heart Rate (bpm)")
     # plt.xlim(120,180)
     # plt.legend(labelspacing=0, frameon=True, fontsize=8)
